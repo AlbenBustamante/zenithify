c3b292bca4071abb2888825492136e4809069d27
Alben Bustamante <albenbustamante@gmail.com>
2026-08-04 23:27:03 -0400

    feat: add ngx-env for environment variable management and update environment configurations
    - Added @ngx-env/builder to package.json for enhanced environment variable handling.
    - Created src/env.d.ts to define types for environment variables.
    - Updated production and development environment files to use import.meta.env for Supabase URL and anon key.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: false,
   supabase: {
-    url: 'https://ekutklrbldwmftuhdeab.supabase.co',
-    anonKey:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrdXRrbHJibGR3bWZ0dWhkZWFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMjM2MDIsImV4cCI6MjA5Mjg5OTYwMn0.zCI0fCO74xeeJMFe2W0LjmvFeRCHxQIWZT45Ha5TOZg',
+    url: import.meta.env['NG_APP_SUPABASE_URL'],
+    anonKey: import.meta.env['NG_APP_SUPABASE_ANON_KEY'],
   },
   dolarApiUrl: 'https://ve.dolarapi.com/v1/dolares',
 };
